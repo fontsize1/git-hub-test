c7176f81f121151e384805dbc03eff7ad78cdaae
fontsize1 <95947825+fontsize1@users.noreply.github.com>
2021-12-10 21:58:51 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,3 +2,6 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     music.playMelody("C5 B - G - B F - ", 120)
 })
 music.baDing.play()
+forever(function () {
+    game.splash("\"sus\"")
+})
