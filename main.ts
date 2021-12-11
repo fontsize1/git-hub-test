e517e9aae034434e37e0246b85911bf790e362db
McBiggie <93778239+McBiggie@users.noreply.github.com>
2021-12-10 22:11:25 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     music.playMelody("C5 B - G - B F - ", 120)
 })
+let mySprite: Sprite = null
 music.baDing.play()
+game.onUpdate(function () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
 forever(function () {
     game.splash("\"sus\"")
 })
